1029bf87680eb1261f13bf03a8c0ea9057a77022
dafe2201 <Daniel_fellden@hotmail.com>
2023-04-25 12:30:20 +0200

    Started implementing tests for Customer. Experiencing a literal ton of bugs. Wiho

--- src/test/java/com/backend1inl/services/impl/CustomerServiceImplTest.java
+++ src/test/java/com/backend1inl/services/impl/CustomerServiceImplTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-@MockitoSettings(strictness = Strictness.LENIENT)
+@MockitoSettings(strictness = Strictness.WARN)
 public class CustomerServiceImplTest {
 
     @Mock
