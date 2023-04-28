409a84b2679bc8ae221de7399394ac0727b82586
andreastif <45339645+andreastif@users.noreply.github.com>
2023-04-28 15:40:07 +0200

    Group session: Custom Exceptions Added, Refactor Controller

---
+++ src/main/java/com/backend1inl/exception/InvalidPriceException.java
@@ -0,0 +1,11 @@
+package com.backend1inl.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class InvalidPriceException extends RuntimeException{
+    public InvalidPriceException(String message){
+        super(message);
+    }
+}
