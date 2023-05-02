43453b4e00b4b46c87c3b8b04c31bb65ddd9eb60
alsomeb <alsomeb@gmail.com>
2023-05-02 12:24:14 +0200

    - refactoring

--- src/test/java/com/backend1inl/TestData.java
+++ src/test/java/com/backend1inl/TestData.java
@@ -2,6 +2,7 @@ package com.backend1inl;
 
 import com.backend1inl.domain.*;
 
+import com.backend1inl.utils.ItemImageUtils;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,7 +13,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+
 
 public class TestData {
 
